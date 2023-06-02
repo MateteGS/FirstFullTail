e893da49545bc035465b29b442cfa09d53c9d5bb
MateteGS <matetematlala98@gmail.com>
2023-06-02 17:49:08 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427dF",
+      }
+    },
+    container: {
+      center: true,
+      DEFAULT: "20px",
+      md: "50px"
+    }
+  },
+  plugins: [],
+}
+
